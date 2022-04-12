38845a2757e1e276f2e5e34efd486ae327bd2c31
Josh Ajiniran <josuajiniran@gmail.com>
2022-04-12 16:29:05 +0100

    remove hardcoded port, add heroku deployment file

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -12,4 +12,4 @@ echo "Postgresql started"
 alembic upgrade head
 
 # Start the server
-uvicorn api.main:app --workers 1 --host 0.0.0.0 --port 8000
+uvicorn api.main:app --workers 1 --host 0.0.0.0 --port $PORT
