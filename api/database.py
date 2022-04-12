24b15b2c01ee98d4481fc268ec366ccd532416dd
Josh Ajiniran <josuajiniran@gmail.com>
2022-04-12 18:01:21 +0100

    remove log

--- api/database.py
+++ api/database.py
@@ -5,9 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from api import config
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@db/dates_facts_db'
-
-print(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = config.DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
