b9f042e9fc711bfe5ece30cf3668f75ee493a285
Josh Ajiniran <josuajiniran@gmail.com>
2022-04-12 17:39:28 +0100

    just add reload to server

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,4 @@ echo "Postgresql started"
 alembic upgrade head
 
 # Start the server
-uvicorn api.main:app --workers 1 --host 0.0.0.0 --port $PORT
+uvicorn api.main:app --reload --workers 1 --host 0.0.0.0 --port $PORT
