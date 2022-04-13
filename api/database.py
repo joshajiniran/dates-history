164d3c8db89f46aa18701c4d6832d51f5ab4181e
Josh Ajiniran <josuajiniran@gmail.com>
2022-04-13 08:53:19 +0100

    fix alembic sqlalchemy for heroku

--- api/database.py
+++ api/database.py
@@ -3,9 +3,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 from api import config
 
-SQLALCHEMY_DATABASE_URL = config.DATABASE_URL.replace("postgres://", "postgresql://", 1)
+SQLALCHEMY_DATABASE_URL = config.DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
